7e4b71d6e05841e59c9a81a9220e3e70debb4dc6
jwats287 <jwats287@users.noreply.github.com>
2017-08-30 13:54:50 -0400

    Posts show up for cities

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,4 +2,13 @@ class CitiesController < ApplicationController
     def index
         @cities= City.all
     end
+
+    def create
+        @city = City.create!(city_params)
+    end
+
+    private
+    def city_params
+        params.require(:city).permit(:name, :photo_url)
+    end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,22 @@
 class PostsController < ApplicationController
+    def index
+        @posts= Post.all
+    end
+    def destroy
+        @post.delete
+        respond_to do |format|
+          format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+          format.json { head :no_content }
+        end
+      end
+    # def create
+    #     @city = City.find(params[:city_id])
+    #     @post = @city.posts.create(post_params)
+    # end
+
+    # private
+    # def post_params
+    #     params.require(:post).permit(:title, :review)
+    # end
+
 end
