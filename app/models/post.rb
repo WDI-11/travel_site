dce17fca1934437ccf1d26ec6cb1a9b0143bc5f6
mwlai288 <mwlai288@gmail.com>
2017-08-31 14:45:07 -0400

    installed devise

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
-    belongs_to :city
+    belongs_to :city 
+    belongs_to :user
     validates_length_of :review, :maximum => 200
     validates_length_of :review, :minimum => 1
 end
