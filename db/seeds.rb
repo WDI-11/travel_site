d6b8c9f2a66eadff8b1aa4f4bf159439f4b0893d
jwats287 <jwats287@users.noreply.github.com>
2017-08-31 12:37:58 -0400

    Styling sorta done. Still need footer

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,12 @@
 Post.destroy_all
 City.destroy_all
 
-atlanta = City.create(name: 'Atlanta', photo_url: 'http://usa.sae.edu/assets/Campuses/Atlanta/2015/Atlanta_city_view.jpg')
-london = City.create(name: 'London', photo_url: 'https://media.timeout.com/images/103042848/image.jpg')
+atlanta = City.create(name: 'Atlanta', photo_url: 'https://i.imgur.com/jf9v1tz.jpg')
+london = City.create(name: 'London', photo_url: 'https://i.imgur.com/MWuCmFY.jpg')
+san_francisco = City.create(name: 'San Francisco', photo_url: 'https://i.imgur.com/SyqwfpR.jpg')
 Post.create(title: 'This place sucks', review: 'It really really sucks', city_id:atlanta.id)
 Post.create(title: 'C# Required', review: 'This place is awesome if you know C#.', city_id:atlanta.id)
 Post.create(title: 'Joey Hurley Is Danny\'s Newest Clone', review: 'In 10-20 years, he will achieve consciousness and kill his older self, take over his life, and repeat the process in a different city.', city_id:atlanta.id)
 Post.create(title: 'Good Place to Panhandle', review: 'I prefer the street across from Ponce City Market, where if the weather is bad, you can still turn people in and get them towed.', city_id:atlanta.id)
 Post.create(title: 'This place sucks too', review: 'It really really sucks', city_id:london.id)
+Post.create(title: 'Lost of unwashed hippies', review: 'Is there no shower here?', city_id: san_franciso.id)
